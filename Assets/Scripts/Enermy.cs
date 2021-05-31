9052833fcb774668ae3a2ceba0eca68824c7e55c
QURARE <75406469+QURARE@users.noreply.github.com>
2021-05-31 14:46:44 +0900

    0531

--- Assets/Scripts/Enermy.cs
+++ Assets/Scripts/Enermy.cs
@@ -6,7 +6,19 @@ public class Enermy : MonoBehaviour
 {
     public Transform target;
     private Rigidbody rig;
+    private bool scored = false;
     public float speed = 150f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet" && scored == false)
+        {
+            scored = true;
+            GameManager.instance.AddScore(1);
+            transform.position = new Vector3(Random.Range(-30, 30), 0.5f, Random.Range(-30, 30));
+            scored = false;
+        }
+    }
 
     void Start()
     {
